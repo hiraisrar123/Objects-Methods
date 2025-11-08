2950140cf1605dfe363aac9cd4f04298cdc2ad1b
hiraisrar123 <hiraisrar47@gmail.com>
2025-11-08 09:35:00 -0800

    hira

--- script.js
+++ script.js
@@ -61,9 +61,22 @@
 // greet();
 // greet('Hira');
 
-const arr1 = [1,2];
-const arr2 = [...arr1, 3, 4];
-console.log(arr2);
-const obj1 = {a: 1};
-const obj2 = {...obj1, b: 2};
-console.log(obj2);
+// const arr1 = [1,2];
+// const arr2 = [...arr1, 3, 4];
+// console.log(arr2);
+// const obj1 = {a: 1};
+// const obj2 = {...obj1, b: 2};
+// console.log(obj2);
+
+// function sum(...numbers){
+//     return numbers.reduce((a, b) => a + b);
+// }
+// console.log(sum(1, 2, 3));
+
+class Person {
+    contractor (name){
+        this.name = name}
+        greet(){
+            return `Hello ${this.name}`;
+        }
+}
