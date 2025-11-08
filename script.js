72044f5e8e17a0f790b2dd9b97550f0cb576b2b3
hiraisrar123 <hiraisrar47@gmail.com>
2025-11-08 20:31:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// const car ={
+//     brand: "Toyota",
+//     model: "Corlla",
+//     year: 2022
+// }
+// console.log(Object.keys(car));
+// console.log(Object.values(car));
+// console.log(Object.entries(car));
+
+// const  obj1 = {a:1 ,b:2};
+// const  obj2  =  {c:3};
+// const merged  = Object.assign({}, obj1, obj2);
+// console.log(merged);
+
+// const  user={name: "Sara",}
+// Object.freeze(user); // No Change
+// console.log(user.name);;
+
+// const  data ={
+//     id: 1 , status:  'active'}
+// Object.seal(data);
+
+// data.status= 'inactive'; _______Allowed
+
+// console.log(data);
+
+// data.newkey  = 'x'______Not  Allowed
+// delete data.id _________Not  Allowed
+
+// const student  ={name:  'Ali'}
+// console.log(student.hasOwnProperty('name')); ________True
+// console.log(student.hasOwnProperty('age')); ________False
+
+// const parent = {greet: function(){
+//     return'Hello!';
+// }}
+// const child = Object.create(parent);
+// console.log(child.greet());
+
+// const arr =[['name' ,"Hira"], ['age' ,19]]
+// const obj = Object.fromEntries(arr);
+// console.log(obj); Array se object banata hai
+
+// const example = {x: 10 , y: 20}
+// console.log(Object.getOwnPropertyNames(example));;
